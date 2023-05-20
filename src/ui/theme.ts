8c779ca9ad3a931202fac0c88f79a60f391293b0
Natallia-Alisevich <105042840+Natali2707@users.noreply.github.com>
2023-05-20 23:04:55 +0300

    feat (ui): add ui (colors)

---
+++ src/ui/theme.ts
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+import { Color } from "ui";
+
+export const lightTheme = css`
+  --primary-text: ${Color.Primary};
+  --primary-bg: ${Color.White};
+  --search-bg: ${Color.White};
+`;
+export const darkTheme = css`
+  --primary-text: ${Color.White};
+  --primary-bg: ${Color.Primary};
+  --search-bg: ${Color.Secondary_lightGrey};
+`;
