a8b7002ae436b105d07a96c3f89f37870fed7ccf
Natallia-Alisevich <105042840+Natali2707@users.noreply.github.com>
2023-05-20 22:57:01 +0300

    feat (hooks, ui): add useDebounce, useToggle, ui (colors)

---
+++ src/routes/hooks/hooks.ts
@@ -0,0 +1,23 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "store/store";
+
+// 1.типизируем useSelector:
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// созд перем-ю useAppSe-r, протип-ли RootState и в эту перем-ю поместили ссылку на ф-ю useSelector
+
+// useAppSelector-это кастом хук, ч! вместо useSelector использовать useAppSelector
+// сразу с типизацией стора чтоб в хоумпэйдж писать сразу вот так:
+
+// const { products } = useAppSelector((state) => state.cart);
+//  вместо:
+
+// const { products } = useSelector((state: RootState) => state.cart);
+
+// т.е. state не надо пост-но тип-ть
+
+//2. тип-ем dispatch:
+const useAppDispatch: () => AppDispatch = useDispatch;
+// здесь происходит: перем-я useAppDispatch б содержать тип в виде ф-ции, а эта ф-я б возвращать AppDispatch
+// AppDispatch-это в сторе мы создали (он типизирует наш диспатч (наши экшины))
+
+export { useAppSelector, useAppDispatch };
