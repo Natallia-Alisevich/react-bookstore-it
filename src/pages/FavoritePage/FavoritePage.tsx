2f2f9292f3aedbf3e9e92e79a6169f97c04ac08c
Natallia-Alisevich <105042840+Natali2707@users.noreply.github.com>
2023-05-20 23:21:30 +0300

    built (project): add pages

---
+++ src/pages/FavoritePage/FavoritePage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { ROUTE } from "routes"
+
+export const FavoritePage = () => {
+  return (
+    <div>
+      <h2>  FavoritePage</h2>
+      <Link to={ROUTE.HOME}>to home</Link>
+    </div>
+  )
+}
