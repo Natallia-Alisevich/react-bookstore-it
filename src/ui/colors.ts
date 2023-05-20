b6f547f8ac6cc8e447ed3733b61d68f79fc4bde4
Natallia-Alisevich <105042840+Natali2707@users.noreply.github.com>
2023-05-20 23:08:38 +0300

    feat (ui): add ui (colors)

---
+++ src/ui/colors.ts
@@ -0,0 +1,17 @@
+export enum Color {
+  Primary = "#313037",
+  Primary_grey = "#5B5A62",
+  Secondary_lightGrey = "#A8A8A8",
+  Grey = "#E7E7E7",
+  Light = "#F7F7F7",
+  White = "#FFFFFF",
+  Red = "#FC857F",
+  Blue = "#D7E4FD",
+  Green = "#CAEFF0",
+  Orange = "#FEE9E2",
+  Purple = " #F4EEFD",
+
+  Primary_Text = "var(--primary-text)",
+  Primary_Bg = "var(--primary-bg)",
+  Search_Bg = "var(--search-bg)",
+}
---
+++ src/ui/index.ts
@@ -0,0 +1,3 @@
+import { Color } from "./colors";
+import { lightTheme, darkTheme } from "./theme";
+export { Color, lightTheme, darkTheme };
